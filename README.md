23e64582446e2db8e8897d861233f7f3a7888d84
Travis Shears <btbtravis@gmail.com>
2020-04-17 23:51:11 +0200

    chore: add styled-components

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ A small project to enable a plesent morning sun rise via IOT lights.
 
 - [create react app with typescript docs](https://create-react-app.dev/docs/adding-typescript)
 - [IBM carbon storybook](https://react.carbondesignsystem.com/?path=/story/radiobutton--default)
+- [styled-components docs](https://styled-components.com/docs/)
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Header, HeaderName } from 'carbon-components-react';
-// import './App.css';
+import { Header, HeaderName, Button } from 'carbon-components-react';
+import styled from 'styled-components';
+
+const MainWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 90vh;
+`;
 
 function App() {
   return (
     <div className="App">
       <Header aria-label="Sunrise App Title">
-        <HeaderName href="/">
-          Sunrise
+        <HeaderName href="/" prefix="">
+          Project Sunrise
         </HeaderName>
       </Header>
+      <MainWrapper>
+        <Button>Hello World</Button>
+      </MainWrapper>
     </div>
   );
 }
