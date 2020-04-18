4a9043988abbcffc67160895c69d31fe3061e687
Steven <stevenjacobprice@gmail.com>
2020-04-18 12:39:32 -0400

    init device list

---
+++ frontend/src/components/DeviceList.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Tabs, Tab } from 'carbon-components-react';
+
+
+const SunRiseDeviceList: React.FC<{ device: Device }> = ({device}) => {
+
+}
+
+export default SunRiseTabs;
