6372677c7b32a6ee38554003554aaa874b4ca085
Travis Shears <btbtravis@gmail.com>
2020-04-18 12:01:30 +0200

    feat(security): force login with key

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,4 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# TODO: get this secret key from .env
+app_secret_key = 'XXXXXXXXXXX'
+app.secret_key = bytes(app_secret_key, 'utf-8')
+
 from app import routes
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,8 +1,36 @@
+from flask import Flask, session, redirect, url_for, request
+from markupsafe import escape
+
 from app import app
 
-cors_fix = { 'Access-Control-Allow-Origin': '*' }
-
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    return ("hello world", cors_fix)
+    # TODO: replace this xxx with key from .env
+    if 'key' in session and session['key'] == 'xxx':
+        return 'You are logged in with the correct key'
+    elif 'key' in session:
+        return '''
+            <h1>Incorrect key</h1>
+            <a href="/login">Return to login</a>
+        '''
+    return redirect(url_for('login'))
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['key'] = escape(request.form['key'])
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=key>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+
+@app.route('/logout')
+def logout():
+    # remove the key from the session
+    session.pop('key', None)
+    return redirect(url_for('index'))
