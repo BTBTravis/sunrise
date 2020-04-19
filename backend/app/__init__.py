ef7c6ffff8a5d8271d011676aa4e22c241dec8de
Travis Shears <btbtravis@gmail.com>
2020-04-19 09:33:06 +0200

    deploy(0.0.1): init deploy system and deploy v0.0.1

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__, static_folder='react_app')
-app.run(debug=True, use_reloader=True)
+# app.run(debug=True, use_reloader=True)
 
 
 app_secret_key = os.environ['APP_SECRET_KEY']
